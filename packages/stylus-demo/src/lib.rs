8ea975e21a0379bb390fe430d70582a5acfd5229
0xbhumi <bhumisadariya@lampros.tech>
2025-08-21 17:29:46 -0400

    updated counter code

--- packages/stylus-demo/src/lib.rs
+++ packages/stylus-demo/src/lib.rs
@@ -24,6 +24,7 @@
 // Allow `cargo stylus export-abi` to generate a main function.
 #![cfg_attr(not(feature = "export-abi"), no_main)]
 extern crate alloc;
+use stylus_cache_sdk::{is_contract_cacheable};
 
 /// Import items from the SDK. The prelude contains common traits and macros.
 use stylus_sdk::{alloy_primitives::U256, prelude::*};
@@ -65,4 +66,8 @@ impl Counter {
         let number = self.number.get();
         self.set_number(number + U256::from(1));
     }
+    pub fn is_cacheable(&self) -> bool {
+        is_contract_cacheable()
+    }
+
 }
